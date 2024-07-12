a1ad3bcb2173f313741253e020671cfb50752638
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2024-07-13 01:07:28 +0200

    wip: sponsor transaction with pimlico paymaster

--- game/src/client/react/ConnectButton.jsx
+++ game/src/client/react/ConnectButton.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import {
     useDynamicContext,
     DynamicConnectButton,
+    DynamicWidget,
     useUserWallets,
 } from "@dynamic-labs/sdk-react-core";
-import { zeroAddress } from "viem";
+import {
+    createSmartAccountClient,
+    walletClientToSmartAccountSigner,
+} from "permissionless";
+import { signerToSimpleSmartAccount } from "permissionless/accounts";
+import { createPimlicoPaymasterClient } from "permissionless/clients/pimlico";
+import { zeroAddress, http } from "viem";
+import { useWalletClient } from "wagmi";
 
 const ConnectButton = () => {
     const { isAuthenticated, primaryWallet } = useDynamicContext();
     const wallets = useUserWallets();
+    const { data: walletClient } = useWalletClient();
 
     console.log({
         isAuthenticated,
@@ -31,24 +40,63 @@ const ConnectButton = () => {
                 </DynamicConnectButton>
             ) : (
                 <>
-                    <p>{primaryWallet?.address}</p>
+                    <DynamicWidget
+                        buttonClassName="flex flex-row justify-center w-full"
+                        buttonContainerClassName="flex flex-row justify-center w-full"
+                    />
                     <button
                         id="playButton"
                         className={`relative mx-auto mt-2 w-full h-10 box-border text-lg text-white text-center shadow-inner bg-green-500 border-b-2 border-green-600 cursor-pointer rounded mb-2 hover:bg-green-400`}
                         onClick={async () => {
+                            const pimlicoKey =
+                                "3fac7f38-a857-468a-a471-20f077048d26";
+
+                            const chain = walletClient.chain;
+
+                            const pimlicoUrl = `https://api.pimlico.io/v2/${chain.id}/rpc?apikey=${pimlicoKey}`;
+
                             try {
-                                const client =
-                                    await primaryWallet?.connector.getSigner();
+                                const customSigner =
+                                    walletClientToSmartAccountSigner(
+                                        walletClient
+                                    );
 
-                                console.log({ client });
+                                const pimlicoPaymasterClient =
+                                    createPimlicoPaymasterClient({
+                                        chain,
+                                        transport: http(pimlicoUrl),
+                                        entryPoint:
+                                            "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+                                    });
 
-                                const hash = await client?.sendTransaction({
+                                const simpleSmartAccountClient =
+                                    await signerToSimpleSmartAccount(
+                                        walletClient,
+                                        {
+                                            entryPoint:
+                                                "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789",
+                                            signer: customSigner,
+                                            factoryAddress:
+                                                "0x9406Cc6185a346906296840746125a0E44976454",
+                                        }
+                                    );
+
+                                const smartAccountClient =
+                                    createSmartAccountClient({
+                                        account: simpleSmartAccountClient,
+                                        chain, // or whatever chain you are using
+                                        transport: http(pimlicoUrl),
+                                        sponsorUserOperation:
+                                            pimlicoPaymasterClient.sponsorUserOperation, // if using a paymaster
+                                    });
+
+                                await smartAccountClient.sendTransaction({
                                     to: zeroAddress,
                                     data: "0x0",
                                     value: BigInt(1),
                                 });
 
-                                alert(hash);
+                                // alert(hash);
                             } catch (error) {
                                 console.log(error);
                             }
--- game/src/client/react/Provider.jsx
+++ game/src/client/react/Provider.jsx
@@ -1,6 +1,23 @@
 import React from "react";
 import { DynamicContextProvider } from "@dynamic-labs/sdk-react-core";
 import { EthereumWalletConnectors } from "@dynamic-labs/ethereum";
+import { DynamicWagmiConnector } from "@dynamic-labs/wagmi-connector";
+import { createConfig, WagmiProvider, useAccount } from "wagmi";
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { http } from "viem";
+import { base, sepolia } from "viem/chains";
+
+const config = createConfig({
+    chains: [sepolia, base],
+    multiInjectedProviderDiscovery: false,
+    transports: {
+        [sepolia.id]: http(),
+        [base.id]: http(),
+    },
+});
+
+const queryClient = new QueryClient();
 
 const Provider = ({ children }) => {
     return (
@@ -8,9 +25,14 @@ const Provider = ({ children }) => {
             settings={{
                 environmentId: "c8ca5061-e827-44b4-a836-333ed59bd5a4",
                 walletConnectors: [EthereumWalletConnectors],
+                walletConnectors: [EthereumWalletConnectors],
             }}
         >
-            {children}
+            <WagmiProvider config={config}>
+                <QueryClientProvider client={queryClient}>
+                    <DynamicWagmiConnector>{children}</DynamicWagmiConnector>
+                </QueryClientProvider>
+            </WagmiProvider>
         </DynamicContextProvider>
     );
 };
