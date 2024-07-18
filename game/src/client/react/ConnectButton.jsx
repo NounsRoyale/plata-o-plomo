960f9a47b8a272b1426639a86f3f5eaab9d33d65
Bruno Eleodoro Roza <brunoeleodoro96@gmail.com>
2024-07-18 09:53:42 -0300

    play without rewards working

--- game/src/client/react/ConnectButton.jsx
+++ game/src/client/react/ConnectButton.jsx
@@ -105,22 +105,22 @@ const ConnectButton = () => {
                         ) : isLoading || !isReady ? (
                             <div className="flex justify-center">
                                 <svg
-                                    class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+                                    className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
                                     xmlns="http://www.w3.org/2000/svg"
                                     fill="none"
                                     viewBox="0 0 24 24"
                                     id="isLoading"
                                 >
                                     <circle
-                                        class="opacity-25"
+                                        className="opacity-25"
                                         cx="12"
                                         cy="12"
                                         r="10"
                                         stroke="currentColor"
-                                        stroke-width="4"
+                                        strokeWidth="4"
                                     ></circle>
                                     <path
-                                        class="opacity-75"
+                                        className="opacity-75"
                                         fill="currentColor"
                                         d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
                                     ></path>
@@ -132,33 +132,41 @@ const ConnectButton = () => {
                             "Play"
                         )}
                     </button>
-                    <br />
-                    <button
-                        id="fakePlayer"
-                        className="relative mx-auto mt-2 w-full h-10 box-border text-lg text-white text-center shadow-inner bg-green-500 border-b-2 border-green-600 cursor-pointer rounded mb-2 hover:bg-green-400"
-                        onClick={() => {
-                            const randomEthAddress =
-                                "0x" + Math.random().toString(16).slice(2);
-                            window.startGame("player", randomEthAddress);
-                        }}
-                    >
-                        Play without rewards
-                    </button>
-                    <br />
-                    <button
-                        id="spectateButton"
-                        className="relative mx-auto mt-2 w-full h-10 box-border text-lg text-white text-center shadow-inner bg-green-500 border-b-2 border-green-600 cursor-pointer rounded mb-2 hover:bg-green-400"
-                    >
-                        Spectate
-                    </button>
-                    <button
-                        id="settingsButton"
-                        className="relative mx-auto mt-2 w-full h-10 box-border text-lg text-white text-center shadow-inner bg-green-500 border-b-2 border-green-600 cursor-pointer rounded mb-2 hover:bg-green-400"
-                    >
-                        Settings
-                    </button>
                 </>
             )}
+            <br />
+            <button
+                id="fakePlayer"
+                className="relative mx-auto mt-2 w-full h-10 box-border text-lg text-white text-center shadow-inner bg-green-500 border-b-2 border-green-600 cursor-pointer rounded mb-2 hover:bg-green-400"
+                onClick={() => {
+                    const randomEthAddress =
+                        "0x" + Math.random().toString(16).slice(2);
+                    global.playerName = randomEthAddress;
+                    // window.startGame("player", randomEthAddress);
+                    // ({ type, playerName, initialFlowRate, balance })
+                    window.startGame({
+                        type: "player",
+                        playerName: randomEthAddress,
+                        initialFlowRate: BigInt(0),
+                        balance: BigInt(0),
+                    });
+                }}
+            >
+                Play without rewards
+            </button>
+            <br />
+            <button
+                id="spectateButton"
+                className="relative mx-auto mt-2 w-full h-10 box-border text-lg text-white text-center shadow-inner bg-green-500 border-b-2 border-green-600 cursor-pointer rounded mb-2 hover:bg-green-400"
+            >
+                Spectate
+            </button>
+            <button
+                id="settingsButton"
+                className="relative mx-auto mt-2 w-full h-10 box-border text-lg text-white text-center shadow-inner bg-green-500 border-b-2 border-green-600 cursor-pointer rounded mb-2 hover:bg-green-400"
+            >
+                Settings
+            </button>
         </>
     );
 };
